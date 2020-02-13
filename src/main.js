06290077dea92990246fbf958c149778734a2293
mariush2 <marius@mh2.no>
2020-02-13 13:08:33 +0100

    build: added basic authentication with the cytomine js-client

--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
 import Vue from 'vue';
+import { Cytomine, User, ProjectCollection } from 'cytomine-client';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// Setup connection to Cytomine
+
 Vue.config.productionTip = false;
+
+async function connectToCytomineServer() {
+  const URL = 'URL';
+  const cytomine = await new Cytomine(URL);
+  // Connect using username and password for now
+  const username = 'USERNAME';
+  const password = 'PWD';
+  await cytomine.login(username, password);
+
+  const user = await User.fetchCurrent();
+  const projects = await ProjectCollection.fetchAll();
+  return { user, projects };
+}
+connectToCytomineServer()
+  .then(({ user, projects }) => {
+    // Add current cytomine related stuff to vuex store
+    store.commit('setUser', { user });
+    store.commit('setProjects', { projects });
+  });
 
 new Vue({
   router,
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,16 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: null,
+    projects: null,
   },
   mutations: {
+    setUser(state, payload) {
+      state.user = payload.user;
+    },
+    setProjects(state, payload) {
+      state.projects = payload.projects;
+    },
   },
   actions: {
   },
