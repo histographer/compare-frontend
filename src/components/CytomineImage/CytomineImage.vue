409ab81ebc6abb4e611f994ec4ab3bae126d7231
mariush2 <marius@mh2.no>
2020-02-13 13:09:40 +0100

    refactor: removed unnecessary components from boilerplate

---
+++ src/components/CytomineImage/CytomineImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    this is an image
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CytomineImage',
+};
+</script>
+
+<style scoped>
+  div {
+    border: 2px solid black;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CytomineImage />
+    <CytomineImage />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import CytomineImage from '../components/CytomineImage/CytomineImage.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    CytomineImage,
   },
 };
 </script>
