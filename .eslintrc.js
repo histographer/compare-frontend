39466c4664c1f2536cca6b42c89562310e29e49d
Tor Berre <tor0405@hotmail.no>
2020-02-18 15:36:32 +0100

    fix: downgrade packages to remove breaking conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,10 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "vue/return-in-computed-property": "off",
+    "vue/no-unused-components": "off",
+    "no-prototype-builtins": "off",
+    "no-unused-vars": "off"
   },
   parserOptions: {
     parser: 'babel-eslint',
