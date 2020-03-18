cbbf796e761d676c53d59552191212bad84af011
mariush2 <mariush2@github.com>
2020-03-18 14:00:23 +0100

    fix: fix baseURL:)

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ Vue.config.productionTip = false;
 
 
 // Setting the baseURL (middleware)
-store.commit('changeBaseURL', { newUrl: 'localhost:9292' });
+store.commit('changeBaseURL', { newUrl: 'compare-api.digipat.no' });
 
 new Vue({
   router,
